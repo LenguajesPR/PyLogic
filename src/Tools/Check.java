57d66fef192cdec5faeddd6a2c354a4f52be1eb0
gebejaranod <gabriel.ebd3@gmail.com>
2017-11-26 19:00:19 -0500

    1

--- src/Tools/Check.java
+++ src/Tools/Check.java
@@ -6,20 +6,20 @@ import java.util.LinkedList;
 public class Check {
     LinkedList<HashMap<Object,Object> > tablas = new LinkedList<>();
     HashMap<Object, Object> table = new HashMap<>();
+    static final int ID =1,ENTERO=2,FLOAT=3,IMAG=4,BINARIO=5,HEXA=6,OCTAL=7,TRUE=8,FALSE=9,STRING=10,BITS=11,NONE=12,OTHER=13;
     
-    public boolean validar(Object tabla, Node nodo){
+    public Object validar(Object tabla, Node nodo){
         tablas = (LinkedList<HashMap<Object,Object> >)tabla;
         for(int i = 0; i < tablas.size(); i++){
             table = tablas.get(i);
             if(table.get(nodo) != null){
-                return true;
+                return table.get(nodo);
             }
         }
         /// return error
-        return false;
+        return null;
     } 
     
-    public Check() {
-    }
-        
+    
+       
 }
